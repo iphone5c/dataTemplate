52c438642a94dac178456af4e542c96e0130097e
weiyuan <1612239320@qq.com>
2016-06-26 16:10:55 +0800

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         List<Table> tableList= XmlDatasFactory.getAllTableList("E:\\dataTemplate\\src\\main\\resources\\templateConfig.xml");
         System.out.println(tableList);
+        System.out.println("hello world");
     }
 }
