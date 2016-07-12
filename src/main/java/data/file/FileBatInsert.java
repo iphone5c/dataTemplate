bfcceca46a7bb251f7a9fe46c342a3519408bdc6
huangshumei <975544096@qq.com>
2016-07-12 15:19:49 +0800

    添加

--- src/main/java/data/file/FileBatInsert.java
+++ src/main/java/data/file/FileBatInsert.java
@@ -1,23 +1,59 @@
-package data.jdbc;
+package data.file;
 
-import data.core.Column;
 import data.core.Table;
 import data.utils.Factory;
 import data.utils.Params;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.SQLExec;
+import org.apache.tools.ant.types.EnumeratedAttribute;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.util.*;
 
 /**
  * Created by wy on 2016/7/8.
  */
-public class JdbcBatInsert {
-    private static Integer batchSize = 10000;
+public class FileBatInsert {
 
-    public static void insertData(Table table,List<Map<String,Object>> list){
+    /**
+     *执行脚本
+     */
+    public static void fileBatDeal(File file){
+        try {
+            SQLExec sqlExec = new SQLExec();
+            //设置数据库参数
+            sqlExec.setDriver(Params.DATA_SOURCE_DRIVER);
+            sqlExec.setUrl(Params.DATA_SOURCE_URL);
+            sqlExec.setUserid(Params.DATA_SOURCE_USER);
+            sqlExec.setPassword(Params.DATA_SOURCE_PASSWORD);
+            //要执行的脚本
+            sqlExec.setSrc(file);
+            //有出错的语句该如何处理
+            sqlExec.setOnerror((SQLExec.OnError)(EnumeratedAttribute.getInstance(SQLExec.OnError.class, "abort")));
+            sqlExec.setPrint(true); //设置是否输出
+            //输出到文件 sql.out 中；不设置该属性，默认输出到控制台
+//            sqlExec.setOutput(new File("resource/sql.out"));
+            sqlExec.setProject(new Project()); // 要指定这个属性，不然会出错
+            sqlExec.execute();
+        } catch (Exception e) {
+            System.out.print("执行文件："+file.getName()+"出错");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取该目录下的文件列表
+     * @param filePath
+     * @return
+     */
+    public static List<File> getFiles( String filePath )
+    {
+        File root = new File( filePath );
+        File[] files = root.listFiles();
+        return Arrays.asList(files);
+    }
+
+   /* public static void insertData(Table table,List<Map<String,Object>> list){
         Connection conn = null;
         PreparedStatement ps = null;
         int result[];
@@ -71,5 +107,5 @@ public class JdbcBatInsert {
         param.deleteCharAt(param.length()-1);
         sb.append(")").append(" values ").append("(").append(param).append(")");
         return sb.toString();
-    }
+    }*/
 }
--- src/main/java/data/utils/Params.java
+++ src/main/java/data/utils/Params.java
@@ -10,23 +10,30 @@ import java.util.concurrent.LinkedBlockingDeque;
  * Created by gene on 2016/6/27.
  */
 public class Params {
+    //数据库参数
     public static String DATA_SOURCE_IP;
     public static String DATA_SOURCE_PORT;
     public static String DATA_SOURCE_USER;
     public static String DATA_SOURCE_PASSWORD;
     public static String DATA_SOURCE_DB;
     public static String DATA_SOURCE_SCHEMA;
+    public static String DATA_SOURCE_URL;
+    public static String DATA_SOURCE_DRIVER;
 
+    //用户部门缓存结果
     public static String queryUserSql = "select * from t_aty_user";
     public static String queryCropSql = "select * from t_aty_corp";
     public static String queryDeptSql = "select * from t_aty_dept";
     public static List<Map<String,Object>> userList;
     public static List<Map<String,Object>> cropList;
     public static List<Map<String,Object>> deptList;
+    public static String sqlDir;
     public static LinkedBlockingDeque<Map<String,List<Map<String,Object>>>> blockingDeque;
 
     public static List<Map<String,Object>> tableList = new ArrayList<>();
 
+    //获取表结构("tableName","insert into tableName (a,b,c) values")
+    public static Map tableMap = new HashMap<String,Object>();
     /**
      * 初始化数据库参数
      */
@@ -42,11 +49,16 @@ public class Params {
             DATA_SOURCE_PASSWORD = readProps.getProperty("dataSource.jdbc.password");
             DATA_SOURCE_DB = readProps.getProperty("dataSource.jdbc.dataBase");
             DATA_SOURCE_SCHEMA = readProps.getProperty("dataSource.jdbc.schema");
+            DATA_SOURCE_URL = readProps.getProperty("dataSource.jdbc.Url");
+            DATA_SOURCE_DRIVER = readProps.getProperty("dataSource.jdbc.driverClass");
 
             userList = DataCache.getDataList(queryUserSql);
             cropList = DataCache.getDataList(queryCropSql);
             deptList = DataCache.getDataList(queryDeptSql);
             blockingDeque=new LinkedBlockingDeque<>();
+//            sqlDir = "/resource/sql";
+            sqlDir = System.getProperty("user.dir").replace("\\","/").concat("/resource/sql");
+//            sqlDir = "E:/idea/dataTemplate" +"/"+ "resource" +"/"+ "sql";
             System.out.println("--------初始化参数完毕-----------");
         }catch (Exception e){
 
