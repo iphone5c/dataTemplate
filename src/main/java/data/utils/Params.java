ea8413f0915420a3936be2c95257b6ca9b3eb674
fengjian <fengjian@qq.com>
2016-07-18 11:01:59 +0800

    删除目录

--- src/main/java/data/utils/Params.java
+++ src/main/java/data/utils/Params.java
@@ -64,11 +64,26 @@ public class Params {
                 //创建目录
                 dir.mkdirs();
             }else {
-                dir.delete();
+                deleteDir(dir);
             }
             System.out.println("--------初始化参数完毕-----------");
         }catch (Exception e){
 
         }
+    }
+
+    private static boolean deleteDir(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            //递归删除目录中的子目录下
+            for (int i=0; i<children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        // 目录此时为空，可以删除
+        return dir.delete();
     }
 }
